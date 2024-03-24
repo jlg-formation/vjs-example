dc990305fc90e7ba52e01b4c4cf16da741f7de33
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-24 10:38:02 +0100

    dev: added fontawesome and views

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Gérez efficacement votre stock&nbsp;!</h1>
+    <RouterLink to="/stock" class="button primary">
+      <span>Voir le stock</span>
+      <FontAwesomeIcon :icon="faAngleRight" />
+    </RouterLink>
   </main>
 </template>
+
+<style scoped>
+main {
+  flex: 0;
+}
+</style>

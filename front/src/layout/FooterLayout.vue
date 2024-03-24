659725d5320f9c9b73267fff49cced15eeed274f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-24 10:33:31 +0100

    dev: added css and layout

---
+++ front/src/layout/FooterLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <RouterLink to="/legal">Mentions Légales</RouterLink>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background: #eee;
+  height: 3em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
---
+++ front/src/layout/HeaderLayout.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">
+      <img src="/logo.svg" alt="Logo" />
+      <span>{{ props.label }}</span>
+    </RouterLink>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background: #eee;
+  height: 3em;
+  padding: 0 0.5em;
+
+  display: flex;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    span {
+      font-weight: bold;
+      font-size: 1.3em;
+    }
+  }
+}
+
+img {
+  height: 2.5em;
+}
+</style>

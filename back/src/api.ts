b084ba0e993269228bc8b073fd071f6836f3c027
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-24 10:19:32 +0100

    dev: added one article more by default

--- back/src/api.ts
+++ back/src/api.ts
@@ -8,6 +8,7 @@ export default app;
 
 let articles: Article[] = [
   { id: "a1", name: "Tournevis", price: 2.99, qty: 45 },
+  { id: "a2", name: "Pelle", price: 5.5, qty: 123 },
 ];
 
 // app.use((req, res, next) => {

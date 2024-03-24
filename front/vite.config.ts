85e7b5a9194d98206e7a69165bd5d949a23e1e29
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-24 10:49:21 +0100

    dev: added proxy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -7,14 +7,18 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
     }
   }
 })
